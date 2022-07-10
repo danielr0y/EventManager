e22d4df5db672392e1439f4da2f2d0468dc17838
danielr0y <jamesroydaniel@gmail.com>
2022-07-10 13:06:35 +1000

    adds all events page

---
+++ EventManager.Web/Controllers/EventsController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventManager.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EventManager.Web.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(
+                new AllEventsViewModel(
+                    new[]
+                    {
+                    new EventPreviewViewModel(
+                        "Some Inactive event",
+                        "n/a",
+                        "n/a",
+                        "Havent really got a descirption for this lazi boi yet",
+                        "inactive",
+                        "0",
+                        "#"
+                    ),
+                    new EventPreviewViewModel(
+                        "Sirromet Wine and Dine",
+                        "from: 30/08/2022 until: 31/08/2022",
+                        "from: 07:00PM until: 08:00PM",
+                        "Three nights a week for 4 weeks over Spring, The Wheel of Brisbane and Sirromet are teaming up to bring you and your partner a dining experience like no other. Leave the kids with uncle Bob and treat your partner to the romantic dinner for two they've been dreaming of.",
+                        "upcoming",
+                        "90",
+                        "#"
+                    ),
+                    new EventPreviewViewModel(
+                        "Sunsuper Riverfire",
+                        "on: 25/09/2022",
+                        "at: 10pm and 11:30pm",
+                        "See Australia's most beautiful city in a new light. As another year of celebrations for our great city draw to a close, Sunsuper and Brisbane Festival will light up the night and you'll have the best seats in the house. Riverfront, sky-high and air-conditioned!",
+                        "upcoming",
+                        "80",
+                        "#"
+                    ),
+                    new EventPreviewViewModel(
+                        "New Year's Eve",
+                        "on: 31/12/2022",
+                        "at: 8pm and 11:30pm",
+                        "New Year's Eve on the Wheel of Brisbane is an experience you won't forget. The most sought after tickets we have on offer, these will sell out fast. Book now",
+                        "upcoming",
+                        "100",
+                        "#"
+                    ),
+                    }
+                )
+            );
+        }
+
+    }
+}
+
---
+++ EventManager.Web/Models/AllEventsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EventManager.Web.Models
+{
+	public class AllEventsViewModel
+	{
+		public AllEventsViewModel(IEnumerable<EventPreviewViewModel> events)
+		{
+			this.Heading = "All Events";
+			this.Events = events;
+		}
+
+        public string Heading { get; }
+        public IEnumerable<EventPreviewViewModel> Events { get; }
+    }
+}
+
