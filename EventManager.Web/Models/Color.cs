a22d18adb55baef486ac96791f98be1e6391a560
danielr0y <jamesroydaniel@gmail.com>
2022-07-18 21:19:51 +1000

    adds messages view model

---
+++ EventManager.Web/Models/Color.cs
@@ -0,0 +1,6 @@
+﻿using EventManager.DomainLayer;
+
+namespace EventManager.Web.Models
+{
+    public enum Color { primary, secondary, success, danger, warning, info, light, dark }
+}
---
+++ EventManager.Web/Models/MessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EventManager.Web.Models
+{
+    public class MessageViewModel
+    {
+        public MessageViewModel(Color category, string body)
+        {
+            this.Category = category;
+            this.Body = body;
+        }
+
+        public Color Category { get; }
+        public string Body { get; }
+    }
+}
